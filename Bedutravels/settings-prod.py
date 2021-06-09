9452d694a8c3185f7b95130d87941533e717339d
au <aupmap@gmail.com>
2021-06-09 11:29:04 -0500

    Cambiando el motor de BD para Mysql en settings-prod.py

--- Bedutravels/settings-prod.py
+++ Bedutravels/settings-prod.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'Bedutravels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aupm$Bedutravels',
+        'USER': 'aupm',
+        'PASSWORD': 'pythonsql',
+        'HOST': 'aupm.mysql.pythonanywhere-services.com'
     }
 }
 
